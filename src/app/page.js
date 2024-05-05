bb2cda8c4898b1f82c30cf0139d6a870bb41036d
Nitant <nitantgupta08@gmail.com>
2024-05-06 01:20:53 +0530

    data map on page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import JobCard from "@/components/JobCard";
 
-const getData = () => {
+const getData = async () => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
@@ -16,10 +17,10 @@ const getData = () => {
     body,
   };
   try {
-    return fetch(
+    return await fetch(
       "https://api.weekday.technology/adhoc/getSampleJdJSON",
       requestOptions
-    ).then((response) => response.text());
+    ).then((response) => response.json());
   } catch (error) {
     console.error(error);
     return { error: "Something went wrong!" };
@@ -28,5 +29,29 @@ const getData = () => {
 
 export default async function Home() {
   const data = await getData();
-  return <main></main>;
+  return (
+    <main
+      style={{
+        width: "100%",
+        flexDirection: "column",
+        display: "flex",
+        gap: "10px",
+        alignItems: "center",
+      }}
+    >
+      <div>Filters</div>
+      <div
+        style={{
+          maxWidth: "1200px",
+          display: "flex",
+          gap: "20px",
+          flexWrap: "wrap",
+        }}
+      >
+        {data?.jdList?.map((l) => (
+          <JobCard data={l} key={l?.jbUid} />
+        ))}
+      </div>
+    </main>
+  );
 }
